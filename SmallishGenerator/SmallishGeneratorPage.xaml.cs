4de0b8db9a3b4b31f6f2c78c42be7ed4c1c4e117
Jonsal <jonsal@gmail.com>
2017-03-03 11:43:53 +0100

    First commit

---
+++ SmallishGenerator/SmallishGeneratorPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace SmallishGenerator
+{
+	public partial class SmallishGeneratorPage : ContentPage
+	{
+		public SmallishGeneratorPage()
+		{
+			InitializeComponent();
+		}
+
+		void OnButtonClicked(object sender, EventArgs args)
+		{
+			result.Text = Translate(entry.Text);
+		}
+
+		private static String Translate(String swedishText)
+		{
+			//Add to this list!
+			string[][] stringsToReplace = new[]
+			{
+				new[] { "s", "ss" },
+				new[] { "r", "" },
+				new[] { "å", "åå" },
+				new[] { "dagen", "dan" }
+			};
+
+			String smallishText = swedishText;
+			foreach (string[] item in stringsToReplace)
+				smallishText = smallishText.Replace(item[0], item[1]);
+
+			return smallishText;
+		}
+	}
+}
